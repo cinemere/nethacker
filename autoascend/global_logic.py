00ea99f42421707079416cc9e84a21fcb5d376ab
cinemere <albina.klepach@gmail.com>
2026-08-29 20:39:24 +0300

    nethackers evolve win

--- autoascend/global_logic.py
+++ autoascend/global_logic.py
@@ -515,10 +515,10 @@ class GlobalLogic:
         while 1:
             explore_stairs_condition = lambda: False
             if self.milestone == Milestone.BE_ON_FIRST_LEVEL:
-                # hypothesis: Tourists that have reached XL7 gain more progression by leaving the exhausted
-                # first floor than by spending thousands of food-burning turns searching there for XL8.
-                condition = lambda: self.agent.blstats.experience_level >= (
-                    7 if self.agent.character.role == Character.TOURIST else 8)
+                # hypothesis: Tourists that have reached XL7 gain more progression by descending for
+                # richer fights than by spending thousands more turns farming dungeon level 1 for XL8.
+                condition = lambda: self.agent.blstats.experience_level >= \
+                    (7 if self.agent.character.role == Character.TOURIST else 8)
                 # explore_stairs_condition = lambda: self.agent.inventory.items.total_nutrition() == 0 and \
                 #                                    self.agent.blstats.hunger_state >= Hunger.NOT_HUNGRY
                 level = (Level.DUNGEONS_OF_DOOM, 1)
